26982ae2f536c7e76d6c2f77096c86cd90c5346a
Lee-sung-il <wj132612@naver.com>
2025-04-16 11:11:31 +0900

    Styled Componets 과제

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
-import "./App.scss";
+import styled from "styled-components";
+// import "./App.scss";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
+import { flexMixin } from "./style/style";
+// section
+
+const ContentsContainer = styled.section`
+  ${flexMixin({ justify: "center", align: "center", wrap: "wrap", gap: 20 })}
+  padding: 20px 40px;
+`;
 
 function App() {
-  return (
-    <main>
-      <Header />
-      <section>
-        {contents.map((el) => (
-          <Content key={el.id} content={el} />
-        ))}
-      </section>
-    </main>
-  );
+    return (
+        <>
+            <Header />
+            <ContentsContainer>
+                {contents.map((el) => (
+                    <Content key={el.id} content={el} />
+                ))}
+            </ContentsContainer>
+        </>
+    );
 }
 
 export default App;
